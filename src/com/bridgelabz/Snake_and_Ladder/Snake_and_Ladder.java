78e6de9019cf9d529c2f7119a7cc9fe56dc67bd4
PremKumbhar <kumbharprem7738@gmail.com>
2022-08-07 18:19:46 +0530

    [Prem]:Added checks a option no play,ladder or snake file

--- src/com/bridgelabz/Snake_and_Ladder/Snake_and_Ladder.java
+++ src/com/bridgelabz/Snake_and_Ladder/Snake_and_Ladder.java
@@ -1,13 +1,33 @@
 package com.bridgelabz.Snake_and_Ladder;
 
-public class Snake_and_Ladder {
+public class Snake_and_Ladder 
+{
 
 	public static void main(String[] args) 
 	{
-		int i = (int) (Math.floor(Math.random() * 10) % 6)+1;
-		{
-		 System.out.println("player rolls die and gets value =" +i);
-		}
+		int i=0;
+		int noPlay=0;
+		int ladder=1;
+		int snake=2;
+			while(i<=20)
+			{
+				int roll = (int) Math.floor((Math.random() * 10) % 3);
+				int value = (int) Math.floor((Math.random() * 10) % 6)+1;
+				System.out.println("Dice Roll =" + value);
+				if(roll == noPlay) 
+				{
+					System.out.println("No Play" + i);
+				}
+				else if (roll == ladder) 
+				{	
+					i=i+value;
+					System.out.println("Ladder" + i);
+				}
+	    	   else 
+	    	   {
+				i=i-value;
+				System.out.println("snake" + i);
+			   }
+			}
 	}
-
 }
