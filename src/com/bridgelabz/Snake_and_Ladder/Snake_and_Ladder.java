bf07cfebda8a9775bacb587475152d07f70c454a
PremKumbhar <kumbharprem7738@gmail.com>
2022-08-07 18:25:24 +0530

    [Prem]:Added repeat till player reaches winning position file

--- src/com/bridgelabz/Snake_and_Ladder/Snake_and_Ladder.java
+++ src/com/bridgelabz/Snake_and_Ladder/Snake_and_Ladder.java
@@ -9,11 +9,13 @@ public class Snake_and_Ladder
 		int noPlay=0;
 		int ladder=1;
 		int snake=2;
-			while(i<=20)
+			while(i<=100)
 			{
 				int roll = (int) Math.floor((Math.random() * 10) % 3);
 				int value = (int) Math.floor((Math.random() * 10) % 6)+1;
 				System.out.println("Dice Roll =" + value);
+				
+				//logic
 				if(roll == noPlay) 
 				{
 					System.out.println("No Play" + i);
@@ -25,9 +27,19 @@ public class Snake_and_Ladder
 				}
 	    	   else 
 	    	   {
-				i=i-value;
-				System.out.println("snake" + i);
-			   }
-			}
+				     i=i-value;
+				     if(i<0)
+				     {
+				     i=0;
+					       System.out.println("snake" + i);
+				     }
+				     else
+				     {
+				    	System.out.println("snake" + i);
+				     }
+	    	   }
+				
+			 
+	     }
 	}
 }
